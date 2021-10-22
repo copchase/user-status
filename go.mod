658ad866db017a704ad461e9a172e7a91bb033e9
copchase <lscnr.thc@gmail.com>
2021-10-21 20:58:21 -0700

    Add Twitch IRC client

---
+++ go.mod
@@ -0,0 +1,8 @@
+module main
+
+go 1.17
+
+require (
+	github.com/gempir/go-twitch-irc/v2 v2.7.0
+	github.com/julienschmidt/httprouter v1.3.0
+)
---
+++ irc.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gempir/go-twitch-irc/v2"
+)
+
+type TwitchUserTracker struct {
+	Name      string
+	Id        string
+	Timestamp time.Time
+}
+
+type UserInfo struct {
+	Name   string `json:"name"`
+	Id     string `json:"id"`
+	Online bool   `json:"online"`
+}
+
+type TwitchUserDatabase struct {
+	Client  *twitch.Client
+	UserMap map[string]map[string]TwitchUserTracker
+}
+
+func GetTwitchClient() *TwitchUserDatabase {
+	client := twitch.NewAnonymousClient()
+	channelList := strings.Split(os.Getenv("TWITCH_CHANNELS"), ",")
+	userMap := make(map[string]map[string]TwitchUserTracker)
+	client.Join(channelList...)
+	client.OnPrivateMessage(createPrivateMsgCallback(userMap))
+
+	err := client.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	return &TwitchUserDatabase{
+		Client:  client,
+		UserMap: userMap,
+	}
+}
+
+func createPrivateMsgCallback(userMap map[string]map[string]TwitchUserTracker) func(twitch.PrivateMessage) {
+	return func(msg twitch.PrivateMessage) {
+		channelName := msg.Channel
+		if value, exists := userMap[channelName]; !exists {
+			// channel doesn't exist on the map yet, make it
+			userMap[channelName] = make(map[string]TwitchUserTracker)
+		} else {
+			userName := msg.User.Name
+			userTracker := TwitchUserTracker{
+				Name:      userName,
+				Id:        msg.User.ID,
+				Timestamp: msg.Time,
+			}
+			value[userName] = userTracker
+		}
+
+	}
+}
+
+func (db *TwitchUserDatabase) GetUserInfo(channel, user string) UserInfo {
+	userMap := db.UserMap
+	if value, exists := userMap[channel]; !exists {
+		return UserInfo{
+			Online: false,
+		}
+	} else {
+		twitchInfo := value[user]
+		duration, err := time.ParseDuration("-15m")
+		if err != nil {
+			return UserInfo{
+				Online: false,
+			}
+		}
+
+		return UserInfo{
+			Name:   twitchInfo.Name,
+			Id:     twitchInfo.Id,
+			Online: twitchInfo.Timestamp.After(time.Now().Add(duration)),
+		}
+	}
+}
